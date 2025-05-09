6997af7fcf51ea95d48ac314cb752ef7a60ca620
laila2005 <laila.mohamed.fikry@gmail.com>
2025-05-09 10:19:01 -0700

    task9

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,12 +1,10 @@
-""" this module inserts a new document in a collection based on kwargs"""
+#!/usr/bin/env python3
+'''Task 9's module.
+'''
+
+
 def insert_school(mongo_collection, **kwargs):
-    """Inserts a new document in the collection based on kwargs
-
-    Args:
-        mongo_collection: pymongo collection object
-        **kwargs: key-value pairs representing the document
-
-    Returns:
-        The _id of the inserted document
-    """
-    return mongo_collection.insert_one(kwargs).inserted_id
+    '''Inserts a new document in a collection.
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
